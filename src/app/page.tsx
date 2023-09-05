8017ba1a544138dd6c2e50ef7a0a8110083b1a30
guicoelhodev <gs.coelho@outlook.com>
2023-09-05 15:10:14 -0300

    feat: remove unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { LinkedList } from 'src/components/LinkedList';
 import { Skeleton } from 'src/components/Skeleton';
 
 export default function Home() {
   return (
     <main>
       {/* <Skeleton height="50px" /> */}
-      <LinkedList />
     </main>
   );
 }
